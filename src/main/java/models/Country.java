e4a6cc289b638f7b8c0b58f0fcffd4050a090bfa
Boryana1 <boryana.dimova1@gmail.com>
2023-01-12 09:07:29 -0600

    Created and added Relation Policy

--- src/main/java/models/Country.java
+++ src/main/java/models/Country.java
@@ -1,2 +1,33 @@
-package models;public class Country {
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+//@Data helps Lombok create Getters & Setters
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String code;
+    private String capital;
+    private String description;
+    private String nationality;
+    private String continent;
+
+    @OneToMany(mappedBy = "country")
+    private List<State> states;
+
 }
+
