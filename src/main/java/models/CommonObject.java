b97d44c49424f353521a5fc0b273efa596766472
Boryana1 <boryana.dimova1@gmail.com>
2023-01-12 09:05:03 -0600

    Created

--- src/main/java/models/CommonObject.java
+++ src/main/java/models/CommonObject.java
@@ -1,2 +1,19 @@
-package models;public class CommonObject {
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class CommonObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String description;
+    private String details;
 }
