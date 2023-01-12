8d1305bb85fb3c0add4ddee40a156cc16af1ba81
Boryana1 <boryana.dimova1@gmail.com>
2023-01-12 09:04:19 -0600

    Created the Application Controller

--- src/main/java/com/fleetmanagement/fleetapp/ApplicationController.java
+++ src/main/java/com/fleetmanagement/fleetapp/ApplicationController.java
@@ -1,2 +1,14 @@
-package com.fleetmanagement.fleetapp;public class ApplicationController {
+package com.fleetmanagement.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String goHome() {
+        return "index";
+    }
+
 }
