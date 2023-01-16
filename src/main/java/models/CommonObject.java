edc6d5a9929979c742b81550c8de777734f5705b
Boryana1 <boryana.dimova1@gmail.com>
2023-01-16 00:45:44 -0600

    Created

--- src/main/java/models/CommonObject.java
+++ src/main/java/models/CommonObject.java
@@ -16,4 +16,47 @@ public class CommonObject {
     private Integer id;
     private String description;
     private String details;
+
+    public CommonObject() {
+
+    }
+
+    public CommonObject(Integer id, String description, String details) {
+        this.id = id;
+        this.description = description;
+        this.details = details;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "CommonObject{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", details='" + details + '\'' +
+                '}';
+    }
 }
