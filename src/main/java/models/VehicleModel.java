1041cf627e68611e64761a5c6687d1fe5cee9afd
Boryana1 <boryana.dimova1@gmail.com>
2023-01-20 10:24:56 -0600

    Done

--- src/main/java/models/VehicleModel.java
+++ src/main/java/models/VehicleModel.java
@@ -1,2 +1,14 @@
-package models;public class VehicleModel {
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class VehicleModel extends CommonObject{
 }
