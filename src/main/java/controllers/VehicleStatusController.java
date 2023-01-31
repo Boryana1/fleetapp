551773b7642cfa40e01711217a934721691947dd
Boryana1 <boryana.dimova1@gmail.com>
2023-01-31 07:07:23 -0600

    Created

--- src/main/java/controllers/VehicleStatusController.java
+++ src/main/java/controllers/VehicleStatusController.java
@@ -1,2 +1,6 @@
-package controllers;public class VehicleStatusController {
+package controllers;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VehicleStatusController {
 }
