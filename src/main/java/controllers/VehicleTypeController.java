3a1773477d2fd0d0d1f306472ba8c5523447649d
Boryana1 <boryana.dimova1@gmail.com>
2023-01-31 07:07:31 -0600

    Created

--- src/main/java/controllers/VehicleTypeController.java
+++ src/main/java/controllers/VehicleTypeController.java
@@ -1,2 +1,6 @@
-package controllers;public class VehicleTypeController {
+package controllers;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class VehicleTypeController {
 }
