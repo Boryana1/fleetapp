6a006b39319f3a5105c49a1d3ae6854fe53fe60c
Boryana1 <boryana.dimova1@gmail.com>
2023-02-11 10:42:52 -0600

    Added a table printing a list the countries and a button to add a country

--- src/main/java/com/fleetmanagement/fleetapp/controllers/CountryController.java
+++ src/main/java/com/fleetmanagement/fleetapp/controllers/CountryController.java
@@ -1,14 +1,35 @@
 package com.fleetmanagement.fleetapp.controllers;
 
+import com.fleetmanagement.fleetapp.models.Country;
+import com.fleetmanagement.fleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries() {
-        return "Country";
+    public String getCountries(Model model) {
+
+        List<Country> countryList = countryService.getCountries();
+
+        model.addAttribute("countries", countryList);
+
+        return "country";
+    }
+
+    @PostMapping("/countries/addNew")
+    public String addNew(Country country){
+        countryService.save(country);
+        return "redirect:/countries";
     }
 
 }
--- src/main/java/com/fleetmanagement/fleetapp/services/CountryService.java
+++ src/main/java/com/fleetmanagement/fleetapp/services/CountryService.java
@@ -1,6 +1,24 @@
 package com.fleetmanagement.fleetapp.services;
+import com.fleetmanagement.fleetapp.models.Country;
+import com.fleetmanagement.fleetapp.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
+
+//    save new country
+    public void save(Country country){
+        countryRepository.save(country);
+    }
+
 }
