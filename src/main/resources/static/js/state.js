0fdd2126550047d47bad29267df02a2abfcdeebc
Boryana1 <boryana.dimova1@gmail.com>
2023-02-15 17:16:14 -0600

    Updated the functions for the edit and description buttons

--- src/main/resources/static/js/state.js
+++ src/main/resources/static/js/state.js
@@ -13,11 +13,29 @@ $('document').ready(function(){
             $('#name-edit').val(state.name);
             $('#capital-edit').val(state.capital);
             $('#code-edit').val(state.code);
-//            $('#country-edit').val(state.country);
-            $('#country-edit').val(state.countryid);
+            $('#country-edit').val(state.country.description);
+//            $('#country-edit').val(state.countryid);
         });
         $('#editModal').modal();
-    })
+    });
+
+    	$('table #details-button').on('click',function(event) {
+    		event.preventDefault();
+
+    		var href= $(this).attr('href');
+    		console.log(href);
+
+    		$.get(href, function(state, status){
+    		console.log("inside state status")
+    			$('#id-details').val(state.id);
+    			$('#name-details').val(state.name);
+    			$('#capital-details').val(state.capital);
+    			$('#code-details').val(state.code);
+    			$('#country-details').val(state.country.description);
+    		});
+    		        $('#detailsModal').modal();
+
+    		});
 
     $('table #delete-button').on('click', function(event){
         event.preventDefault();
